a750e7b86af0e1835f21f2a89da4e69017d8fcc5
Ikquilibrium-SG <ikquisamtance10@gmail.com>
2023-09-08 22:05:42 +0100

    second commit

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 const currentDayOfTheWeek = document.querySelector('[data-testid="currentDayOfTheWeek"]')
 const currentTime = document.querySelector('[data-testid="currentUTCTime"]')
 
-document.addEventListener('DOMContentLoaded', function() {
-    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-    const today = new Date()
-    const dayOfWeek = daysOfWeek[today.getUTCDay()]
-    const milliseconds = today.getUTCMilliseconds()
+const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const today = new Date()
+const dayOfWeek = daysOfWeek[today.getUTCDay()]
+const time = today.getTime()
 
-    currentDayOfTheWeek.textContent = dayOfWeek
-    currentTime.textContent = milliseconds
-});
-
+currentDayOfTheWeek.textContent = dayOfWeek
+currentTime.textContent = time
